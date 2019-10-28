97e0c444ddf6365590554bd765ac4e9a20ac4272
Your Name Here <your_email@youremail.com>
2019-10-28 15:10:21 +0600

    Hyper project

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "flex-menu") {
+    x.className += "responsive";
+  } else {
+    x.className = "flex-menu";
+  }
+}
+/* owl carousel */
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
